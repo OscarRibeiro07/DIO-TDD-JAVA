0ace804467b728bd71d0f56d1f2c9ab28ad67e4e
OscarRibeiro07 <negociante2t@gmail.com>
2025-07-26 08:52:26 -0300

    Iniciando no cmd mvn|Types de proj mvn| Tipo dependencias|Tipo escopo

---
+++ 01 - Maven dependency/MavenInicial/src/main/java/com/dio/mvn/project/App.java
@@ -0,0 +1,13 @@
+package com.dio.mvn.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
