3fe053f932381f0a98c31d8c6d3b68c27e94267d
OscarRibeiro07 <negociante2t@gmail.com>
2025-07-27 10:33:26 -0300

    Utilizando before e after no teste para ser executado antes de todos os outros testes e apos executar os outros testes

--- 03-Testes Unitarios JUNIT/CursoJUnit/src/test/java/br/com/junit/dio/ConsultaDadosDePessoaTeste.java
+++ 03-Testes Unitarios JUNIT/CursoJUnit/src/test/java/br/com/junit/dio/ConsultaDadosDePessoaTeste.java
@@ -1,4 +1,28 @@
 package br.com.junit.dio;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class ConsultaDadosDePessoaTeste {
+    // metodo que é executado antes de tudo
+    @BeforeAll
+    static void ConfiguraConexap(){
+        BancoDeDados.iniciarConexao();
+        System.out.println("Iniciou a conexão com banco");
+
+    }
+
+    @Test
+    void validarDAdosRetorno(){
+        Assertions.assertTrue(true);
+    }
+
+    // metodo que é executado depois de tudo
+    @AfterAll
+    static void FinalizaConexao(){
+        BancoDeDados.finaliazConexao();
+        System.out.println("Finalizou a conexão do banco");
+    }
 }
