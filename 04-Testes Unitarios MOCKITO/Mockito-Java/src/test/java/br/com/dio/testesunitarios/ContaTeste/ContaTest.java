6244f578072c076a56e7cfe003dacbd8233953af
OscarRibeiro07 <negociante2t@gmail.com>
2025-07-29 05:13:15 -0300

    utiliza o spy e inorder para verificar ordem de chamada de metodo

---
+++ 04-Testes Unitarios MOCKITO/Mockito-Java/src/test/java/br/com/dio/testesunitarios/ContaTeste/ContaTest.java
@@ -0,0 +1,29 @@
+package br.com.dio.testesunitarios.ContaTeste;
+
+import br.com.dio.testesunitatios.domain.Conta;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ContaTest {
+
+    @Spy
+    private Conta conta = new Conta(1500);
+
+    @Test
+    void validarOrdemDeChamada(){
+
+        conta.pagaBoleto(300);
+        // valida se os metodos estão sendo chamdos nesta ordem caso nao
+        // é retonado erro de verificação de ordem informando que esta fora do padrão
+        InOrder inOrder = Mockito.inOrder(conta);
+        inOrder.verify(conta).pagaBoleto(300);
+        inOrder.verify(conta).validaSaldo(300);
+        inOrder.verify(conta).debita(300);
+        inOrder.verify(conta).enviaCreditoParaEmissor(300);
+    }
+}
